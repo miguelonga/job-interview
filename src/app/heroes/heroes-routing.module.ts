da59295691d764c7435bb31e3ce2cb394abdefae
miguelonga <miguelmundiaragones@gmail.com>
2023-04-20 14:19:19 +0200

    added heroes module

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 const routes: Routes = [
-  
+  {
+    path: 'heroes',
+    component: ListComponent
+  }
 ];
 
 @NgModule({
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { ListComponent } from './list/list.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListComponent,
+    DialogBoxComponent
+  ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class HeroesModule { }
